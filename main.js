1de0e4c69ddb831a0bc306d95ff177e03e2e4658
JD <gtx2@protonmail.com>
2019-03-13 16:21:36 +0000

    Programista 2

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $('button').click(function(){
+        $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',function(jsonProgram){
+
+             let programList=$('<ul></ul>');
+            programList.append('<li>'+jsonProgram.imie+'</li>');
+            programList.append('<li>'+jsonProgram.nazwisko+'</li>');
+            programList.append('<li>'+jsonProgram.zawod+'</li>');
+            programList.append('<li>'+jsonProgram.firma+'</li>');
+           
+                $('#dane-programisty').html(programList) 
+       
+
+        });
+
+
+
+    })
+
+
+
+
+
+
+
+
+});
+
+
+
